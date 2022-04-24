6a78a0170e4337eb7ba197158135ad6cbae59c64
Jason Fujii <Jasonfujii@yahoo.com>
2022-04-23 23:03:06 -0700

    small changes

--- PotionSlingers-A/Assets/Scripts/marketController.cs
+++ PotionSlingers-A/Assets/Scripts/marketController.cs
@@ -10,13 +10,21 @@ public class marketController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // potions = GameObject.Find("PotionPile").GetComponent<MartketDeck>();
-        // specials = GameObject.Find("SpecialCardPile").GetComponent<MartketDeck>();
+        potions = GameObject.Find("PotionPile").GetComponent<MarketDeck>();
+        specials = GameObject.Find("SpecialCardPile").GetComponent<MartketDeck>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Sell the player their chosen card
+    public void sell(Player player, Card card) {
+        //check how much card costs
+        //check how many pips player has
+        //if enough -> move card from marketDeck to top of player deck
+        //if not enough -> return message that card is too expensive
     }
 }
